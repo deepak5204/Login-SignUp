b4a3f47639371a39478306cb391420bd9f10d75a
Deepak <pdeepak1255@gmail.com>
2024-02-21 19:51:10 +0530

    create react app for login signup page and create components

---
+++ src/Components/loginSignup/loginSignup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loginSignup.css'
+
+const LoginSignUp = () => {
+    return (
+        <div>
+            <b>hello from longin signup page</b>
+        </div>
+    )
+}
+
+export default LoginSignUp
