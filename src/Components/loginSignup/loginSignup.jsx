bd88e1a31e73a5dc34e77db03703352cd81b8606
Deepak <pdeepak1255@gmail.com>
2024-02-21 20:05:07 +0530

    add three fields for name email and password and set backgrougnd color

--- src/Components/loginSignup/loginSignup.jsx
+++ src/Components/loginSignup/loginSignup.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './loginSignup.css'
+import user_icon from '../Assets/password.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
 
 const LoginSignUp = () => {
     return (
-        <div>
+        <div className='container'>
+            <dib className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </dib>
+            <div className="inputs">
+                <div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" />
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="password" />
+                </div>
+            </div>
+
+            <div className="forgot-password">Lost password? <span>Click here!</span></div>
+
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
+            </div>
+
             <b>hello from longin signup page</b>
         </div>
     )
